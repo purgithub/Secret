5fcabe193ebaea27a9dacd9716eb2a94f61573f4
Purshottam Kamble <kamblepurshottam9@gmail.com>
2020-06-26 16:20:41 +0530

    Hash and salting

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const encrypt= require("mongoose-encryption");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds= 10;
 const app= express();
 
 
@@ -40,31 +40,41 @@ app.get("/register", function(req,res){
 });
 ////////////////////////////////////////////////////Simple validation code Level 1-2////////////////////////////////
 app.post("/register", function(req,res){
-    const newUser = new User({
-     email:req.body.username,
-     password:md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+           });
+           newUser.save(function(err){
+               if(err){
+                   console.log(err);
+               }else{
+                   res.render("secrets");
+               }
+           });
+       
+       
+       });
     });
 
-
-});
+    
 
 app.post("/login", function(req,res){
-const email= req.body.username;
-const password = md5(req.body.password);
 
-User.findOne({email:req.body.username}, function(err,foundUser){
+    
+const email= req.body.username;
+const password =req.body.password;
+User.findOne({email}, function(err,foundUser){
  if (!err){
      if(foundUser){
-         if(foundUser.password === password){
-             res.render("secrets");
-         }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+           if(result === true){
+            res.render("secrets");
+           } // result == true
+        });
+            
+         
      }
  }else{
      res.send(err);
